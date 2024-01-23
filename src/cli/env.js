9c8a6e07373825d5e1faac05e4feae145f33b79c
AleksGF <aleksgf@mail.zp.ua>
2024-01-23 14:56:59 +0200

    feat: add parseEnv func

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,11 @@
 const parseEnv = () => {
-    // Write your code here 
+  const env = process.env;
+
+  for (let key in env) {
+    if (key.startsWith('RSS_')) {
+      console.log(`${key}=${env[key]}`);
+    }
+  }
 };
 
 parseEnv();
