ad2d17e2cadf815a54983fdbde6b0e6943c8e375
AleksGF <aleksgf@mail.zp.ua>
2024-01-23 14:41:13 +0200

    feat: add read func

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,27 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+import fsPromises from 'fs/promises';
+
 const read = async () => {
-    // Write your code here 
+  const folderName = 'files';
+  const fileName = 'fileToRead.txt';
+  const errorMessage = 'FS operation failed';
+
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  const srcPath = path.join(__dirname, folderName, fileName);
+
+  try {
+    const fh = await fsPromises.open(srcPath);
+    const rs = fh.createReadStream();
+    rs.pipe(process.stdout);
+  } catch (error) {
+    if (error.code === 'ENOENT') {
+      throw new Error(errorMessage);
+    } else {
+      console.error(error);
+    }
+  }
 };
 
 await read();
