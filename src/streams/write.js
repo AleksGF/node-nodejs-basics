57eea502ec5a67d07e098f5d0d23c52e3850d234
AleksGF <aleksgf@mail.zp.ua>
2024-01-24 11:46:35 +0200

    feat: add writeable stream func

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,64 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+import fsPromises from 'fs/promises';
+import readline from 'readline';
+
 const write = async () => {
-    // Write your code here 
+  const folderName = 'files';
+  const fileName = 'fileToWrite.txt';
+  const questionText = 'Write something for adding to file (enter "__exit" or press CTRL+C to close):\n';
+  const errorMessage = 'FS operation failed';
+
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  const distPath = path.join(__dirname, folderName, fileName);
+
+  try {
+    const fh = await fsPromises.open(distPath, 'a');
+    const ws = fh.createWriteStream();
+
+    ws.write('>>> Write Stream Started at: ' + new Date().toLocaleString() + '\n');
+
+    const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout,
+    });
+
+    const exit = () => {
+      ws.write('<<< Write Stream Stopped at: ' + new Date().toLocaleString() + '\n' + '\n');
+      rl.close();
+      fh.close();
+      process.exit(1);
+    }
+
+    if (process.platform === "win32") {
+      rl.on("SIGINT", () => {
+        process.emit("SIGINT");
+      })
+    }
+
+    process.on('SIGINT', () => {
+      exit();
+    });
+
+    const ask = (question) => {
+      rl.question(question, (answer) => {
+        if (answer === '__exit') exit();
+
+        ws.write(answer + '\n');
+
+        ask(question);
+      });
+    };
+
+    ask(questionText);
+  } catch (error) {
+    if (error.code === 'ENOENT') {
+      throw new Error(errorMessage);
+    } else {
+      console.error(error);
+    }
+  }
 };
 
 await write();
