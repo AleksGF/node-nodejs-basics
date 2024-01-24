1290078c90120a587d26de18b43785fcc34597e7
AleksGF <aleksgf@mail.zp.ua>
2024-01-24 14:41:50 +0200

    feat: add transform func

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,54 @@
+import { pipeline } from 'stream/promises';
+import { Transform } from 'stream';
+
 const transform = async () => {
-    // Write your code here 
+  const welcomeText = 'Write something for reversing (enter "__exit" or press CTRL+C to close).\n';
+
+  const exit = () => {
+    process.stdout.write('\n' + 'Exiting...');
+    process.exit(1);
+  };
+
+  if (process.platform === "win32") {
+    process.stdin.on("SIGINT", () => {
+      process.emit("SIGINT");
+    });
+  }
+
+  process.on('SIGINT', () => {
+    exit();
+  });
+
+  try {
+    const transformer = new Transform({
+      transform(chunk, encoding, callback) {
+        if(chunk.toString().trim() === '__exit') exit();
+
+        callback(
+          null,
+          `Output: ${chunk
+            .toString()
+            .trim()
+            .split('')
+            .reverse()
+            .join('')
+          }\nNew Input: `,
+        );
+      }
+    });
+
+    process.stdout.write(welcomeText);
+    process.stdout.write('Input: ');
+
+    await pipeline(
+      process.stdin,
+      transformer,
+      process.stdout,
+    );
+  } catch (error) {
+    console.error(error);
+    exit();
+  }
 };
 
 await transform();
