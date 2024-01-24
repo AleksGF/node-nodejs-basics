5513e9f859048b2194b36cc2be3c0c8eb14d5893
AleksGF <aleksgf@mail.zp.ua>
2024-01-24 09:37:52 +0200

    feat: add calcHash func

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,29 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+import fsPromises from 'fs/promises';
+import { createHash } from 'crypto';
+
 const calculateHash = async () => {
-    // Write your code here 
+  const srcFolder = 'files';
+  const fileName = 'fileToCalculateHashFor.txt';
+  const errorMessage = 'FS operation failed';
+
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  const filePath = path.join(__dirname, srcFolder, fileName);
+
+  try {
+    const hash = createHash('sha256');
+    const fh = await fsPromises.open(filePath);
+    const rs = fh.createReadStream();
+    rs.pipe(hash).setEncoding('hex').pipe(process.stdout);
+  } catch (error) {
+    if (error.code === 'ENOENT') {
+      throw new Error(errorMessage);
+    } else {
+      console.error(error);
+    }
+  }
 };
 
 await calculateHash();
