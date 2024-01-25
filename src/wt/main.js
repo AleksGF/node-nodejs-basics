e91398d1ffec5ace1e7e3eea99138509c6c7d073
AleksGF <aleksgf@mail.zp.ua>
2024-01-25 15:03:17 +0200

    feat: add workers tasks

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,40 @@
+import { Worker } from 'worker_threads';
+import os from 'os';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 const performCalculations = async () => {
-    // Write your code here
+  const cpuCount = os.cpus().length;
+  const nth = 10;
+
+  const workerFileName = './worker.js'
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  const workerPath = path.join(__dirname, workerFileName);
+
+  const createWorker = (ind) => new Promise((resolve, reject) => {
+    const worker = new Worker(workerPath, { workerData: ind + nth });
+    worker.on('message', (data) => {
+      resolve({ status: 'resolved', data });
+    });
+    worker.on('error', () => {
+      resolve({status: 'error', data: null});
+    });
+    worker.on('exit', (code) => {
+      if (code !== 0) {
+        reject(new Error(`Worker#${ind} stopped with exit code ${code}`));
+      }
+    });
+  });
+
+  const threads = [...Array(cpuCount).keys()].map((i) => createWorker(i));
+
+  try {
+    const results = await Promise.all(threads);
+    console.log(results);
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 await performCalculations();
